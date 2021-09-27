18b451b76b28eeeffde2aaf81beba3d481dcb4da
SLaguerta <20846911+SLaguerta@users.noreply.github.com>
2021-09-27 16:04:38 -0500

    Create list of artists

---
+++ src/app/mock-artists.ts
@@ -0,0 +1,11 @@
+//Defining a constant list of the artists the Ninja turtles are named after; array of 4 artists 
+//Hopefully can use the Museum API to get the four artists?
+
+import { Artist } from './artist'
+
+export const ARTISTS: Artist[] = [
+    {id: 1, name: 'Leonardo'},
+    {id: 2, name: 'Michelangelo'},
+    {id: 3, name: 'Donatello'},
+    {id: 4, name: 'Raphael'}
+]

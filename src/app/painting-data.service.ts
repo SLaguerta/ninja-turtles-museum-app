9607413dff838bc6a70b75bd3dfa21849a008d0c
SLaguerta <20846911+SLaguerta@users.noreply.github.com>
2021-09-29 16:40:43 -0500

    Create function to get Donatello paintings

--- src/app/painting-data.service.ts
+++ src/app/painting-data.service.ts
@@ -7,4 +7,9 @@ import { HttpClient } from '@angular/common/http';
 export class PaintingDataService {
 
   constructor(private http:HttpClient) { }
+
+  //api call to met museum api to return objectids of Donatello paintings
+  getPaintings() {
+    return this.http.get('https://collectionapi.metmuseum.org/public/collection/v1/search?q=donatello');
+  }
 }

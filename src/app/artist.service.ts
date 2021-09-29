a259ac2ead90d2e919ed5dd73d4ded5390de0b4a
SLaguerta <20846911+SLaguerta@users.noreply.github.com>
2021-09-29 11:10:48 -0500

    Update artist service with method to return list of artists

--- src/app/artist.service.ts
+++ src/app/artist.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Artist } from './artist';
+import { ARTISTS } from './mock-artists';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class ArtistService {
 
   constructor() { }
+
+  getArtists(): Artist[] {
+    return ARTISTS;
+  }
 }
